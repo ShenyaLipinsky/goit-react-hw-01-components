68325cd925761be81d85415585a7242df1d205bd
ShenyaLipinsky <shenyalipinskiy@gmail.com>
2022-06-28 20:31:52 +0300

    oops  fix friend render in list

--- src/components/FriendList/FriendList.jsx
+++ src/components/FriendList/FriendList.jsx
@@ -6,12 +6,25 @@ import { FriendListItem } from './FriendListItem';
 export const FriendList = ({ friends }) => {
   return (
     <Box mt={3} mx="auto" py={3} maxWidth={2} as={'ul'}>
-      <FriendListItem friends={friends} />
+      {friends.map(({ id, avatar, name, isOnline }) => (
+        <FriendListItem
+          key={id}
+          avatar={avatar}
+          name={name}
+          isOnline={isOnline}
+        />
+      ))}
     </Box>
   );
 };
 
-// Тут по идее не обазателен проптайп ? т.к. он проверяется в FriendsListItem
 FriendList.propTypes = {
-  friends: propTypes.array.isRequired,
+  friends: propTypes.arrayOf(
+    propTypes.shape({
+      id: propTypes.number.isRequired,
+      isOnline: propTypes.bool.isRequired,
+      name: propTypes.string.isRequired,
+      avatar: propTypes.string,
+    }).isRequired
+  ).isRequired,
 };
--- src/components/FriendList/FriendListItem.jsx
+++ src/components/FriendList/FriendListItem.jsx
@@ -1,4 +1,3 @@
-import propTypes from 'prop-types';
 import {
   FriendsItem,
   StatusOnline,
@@ -7,25 +6,12 @@ import {
   Name,
 } from './FriendList.styled';
 
-export const FriendListItem = ({ friends }) => {
-  return friends.map(({ id, isOnline, avatar, name }) => {
-    return (
-      <FriendsItem key={id} id={id}>
-        {isOnline ? <StatusOnline /> : <StatusOffline />}
-        <Avatar src={avatar} alt="User avatar" />
-        <Name>{name}</Name>
-      </FriendsItem>
-    );
-  });
-};
-
-FriendListItem.propTypes = {
-  friends: propTypes.arrayOf(
-    propTypes.shape({
-      id: propTypes.number.isRequired,
-      isOnline: propTypes.bool.isRequired,
-      name: propTypes.string.isRequired,
-      avatar: propTypes.string,
-    }).isRequired
-  ).isRequired,
+export const FriendListItem = ({ isOnline, avatar, name }) => {
+  return (
+    <FriendsItem>
+      {isOnline ? <StatusOnline /> : <StatusOffline />}
+      <Avatar src={avatar} alt="User avatar" />
+      <Name>{name}</Name>
+    </FriendsItem>
+  );
 };
